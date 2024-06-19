ffe4066d4eb3ecf400be8c21386758c3a4e6a0d1
Amanda McGilchrist <z5358641@unsw.edu.au>
2024-06-19 15:52:50 +1000

    Navigation bar Build

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,7 +1,19 @@
-
+import { Link } from 'react-router-dom';
+import './styles/navigation.css'
 function Navigation() {
 
-  return (<></>)
+  return (
+    <div className='navigation-container'>
+      <div className='navigation-text-container'>
+        <div className='navigation-title'>CourseMapper</div>
+        <div className='vertical-line'></div>
+        <div className='navigation-list-items'>
+          <div className='nav-list-item-container'><Link to='/' className='nav-list-item'>HOME</Link></div>
+          <div className='nav-list-item-container'><Link to='/courseoutlines' className='nav-list-item'>COURSE OUTLINES</Link></div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navigation;
