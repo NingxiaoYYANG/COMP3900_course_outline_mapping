4eb0b51fa73935747c1d62c0de2b94625309125d
Aster Berida <mbmasher@gmail.com>
2024-06-25 18:32:23 +1000

    Set up testing environment

---
+++ api/tests/test_classification_controller.py
@@ -0,0 +1,18 @@
+import unittest
+from src import *
+
+class ClassifyCLOsFromPdfTestCase(unittest.TestCase):
+    def test(self):
+        pass
+
+class MatchVerbsByAITestCase(unittest.TestCase):
+    def test(self):
+        pass
+
+class ExtractWordsFromCLOTestCase(unittest.TestCase):
+    def test(self):
+        pass
+
+class MatchCLOsByDictTestCase(unittest.TestCase):
+    def test(self):
+        pass
---
+++ api/tests/test_extract_helper.py
@@ -0,0 +1,14 @@
+import unittest
+from src import *
+
+class ExtractCLOsFromPdfTestCase(unittest.TestCase):
+    def test(self):
+        pass
+
+class CourseCodeFromPdfTestCase(unittest.TestCase):
+    def test(self):
+        pass
+
+class ExtractCLOsFromURLTestCase(unittest.TestCase):
+    def test(self):
+        pass
