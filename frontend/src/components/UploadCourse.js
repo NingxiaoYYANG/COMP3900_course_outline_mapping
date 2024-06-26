6deb6006c79709a38837ffdd2a7feb06bdf292d9
eiman81 <53110018+eiman81@users.noreply.github.com>
2024-06-26 10:03:33 +1000

    Basic Structure of upload course outline, not yet interactive

--- frontend/src/components/UploadCourse.js
+++ frontend/src/components/UploadCourse.js
@@ -1,8 +1,28 @@
+import React from 'react';
+import './styles/uploadcourse.css';
 
 
 function UploadCourse() {
 
-  return (<></>)
+  return (<>
+    <div className="container">
+      <div className="container_inner">
+        <h1>Upload</h1>
+        <div className="button_group">
+          <button className="button_outline">Course Outline</button>
+          <button className="button_exam">Exam Paper</button>
+        </div>
+        <div className="upload_form">
+          <i className="fas fa-cloud-upload-alt"></i>
+          <p>Drop file to upload</p>
+          <p>or</p>
+          <button className="button">Browse</button>
+          <p className='max_size_label'>Max file size: 10MB</p>
+          <p className='supported_file_label'>Supported file types: PDF</p>
+        </div>
+      </div>
+    </div>
+  </>)
 }
 
 export default UploadCourse;
