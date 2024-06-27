78a51fb399fcd640f992fa56043439414440c10a
eiman81 <53110018+eiman81@users.noreply.github.com>
2024-06-27 14:17:45 +1000

    switch toggle button basic func

--- frontend/src/components/UploadCourse.js
+++ frontend/src/components/UploadCourse.js
@@ -1,25 +1,62 @@
 import React from 'react';
 import './styles/uploadcourse.css';
+import { useState } from 'react';
 
 
 function UploadCourse() {
+
+  const [selection, setSelection] = useState('courseOutline');
+  const handleSelectionChange = (selection) => {
+    setSelection(selection);
+  }
 
   return (<>
     <div className="container">
       <div className="container_inner">
         <h1>Upload</h1>
         <div className="button_group">
-          <button className="button_outline">Course Outline</button>
-          <button className="button_exam">Exam Paper</button>
+          <button 
+            id="button_outline" 
+            className={selection === 'courseOutline' ? 'active' : ''}
+            onClick={() => handleSelectionChange('courseOutline')}
+          >
+            Course Outline
+          </button>
+          <button 
+            id="button_exam" 
+            className={selection === 'examPaper' ? 'active' : ''}
+            onClick={() => handleSelectionChange('examPaper')}
+          >
+            Exam Paper
+          </button>
         </div>
-        <div className="upload_form">
+
+        {selection === 'courseOutline' && (<div className="upload_form">
           <i className="fas fa-cloud-upload-alt"></i>
           <p>Drop file to upload</p>
           <p>or</p>
           <button className="button">Browse</button>
           <p className='max_size_label'>Max file size: 10MB</p>
           <p className='supported_file_label'>Supported file types: PDF</p>
+        </div>)}
+
+        {selection === 'examPaper' && (
+        <>
+          <div className="upload_form">
+            <i className="fas fa-cloud-upload-alt"></i>
+            <p>Drop file to upload</p>
+            <p>or</p>
+            <button className="button">Browse</button>
+            <p className='max_size_label'>Max file size: 10MB</p>
+            <p className='supported_file_label'>Supported file types: PDF</p>
+          </div>
+        <div className="upload_form">
+          <p>Input Text</p>
+          <textarea></textarea>
         </div>
+        </>
+        )}
+        
       </div>
     </div>
   </>)
