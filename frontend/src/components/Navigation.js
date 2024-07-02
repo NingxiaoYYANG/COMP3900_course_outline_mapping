6968fdb5efa8fa4eb71fac82e42322d2f3752ebd
Justin <justinycareer@gmail.com>
2024-07-02 15:41:26 +1000

    removed uneeded frontend code

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -10,10 +10,8 @@ function Navigation() {
         <div className='navigation-title'>CourseMapper</div>
         <div className='vertical-line'></div>
         <div className='navigation-list-items'>
-
           <div className='nav-list-item-container'><Link to='/' className='nav-list-item'  style={location === '/' ? {borderBottom: '2px solid #FCD3CA'} : {}}>HOME</Link></div>
           <div className='nav-list-item-container'><Link to='/courseoutlines' className='nav-list-item' style={location === '/courseoutlines' ? {borderBottom: '2px solid #FCD3CA'} : {}}>COURSE OUTLINES</Link></div>
-          <div className='nav-list-item-container'><Link to='/courseoutlines/uploadfile' className='nav-list-item' style={location === '/courseoutlines/uploadfile' ? {borderBottom: '2px solid #FCD3CA'} : {}}>UPLOAD PDF</Link></div>
         </div>
       </div>
     </div>
