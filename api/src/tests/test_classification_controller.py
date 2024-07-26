92b09c1a0dcb1eaa3fabf64796806b0b50d736a5
Aster Berida <mbmasher@gmail.com>
2024-07-26 11:40:45 +1000

    Add basic unit tests for classification_controller.py

--- api/src/tests/test_classification_controller.py
+++ api/src/tests/test_classification_controller.py
@@ -46,10 +46,15 @@ class TestMatchCLOsByDict:
 
 class TestCheckVerbs:
     def test_can_identify_verbs(self):
-        pass
+        assert classifier.check_is_verb("identify", "VB")
+    
+    def test_can_identify_known_verbs(self):
+        assert classifier.check_is_verb("construct", "NULL")
+        assert classifier.check_is_verb("analyze", "NULL")
+        assert classifier.check_is_verb("create", "NULL")
     
     def test_can_identify_nonverbs(self):
-        pass
+        assert not classifier.check_is_verb("fakeverb", "NULL")
 
 
 class TestExtractWordsFromCLO:
@@ -65,3 +70,35 @@ class TestExtractWordsFromCLO:
         assert classifier.extract_words_from_clo(clo1) == clo1_expected
         assert classifier.extract_words_from_clo(clo2) == clo2_expected
         assert classifier.extract_words_from_clo(clo3) == clo3_expected
+
+class TestMergeBloomsCount:
+    def test_can_merge_blooms_count(self):
+        count1 = {"Analyse": 1, "Apply": 2, "Create": 4, "Evaluate": 3, "Remember": 3, "Understand": 6}
+        count2 = {"Analyse": 5, "Apply": 4, "Create": 1, "Evaluate": 1, "Remember": 2, "Understand": 0}
+        merged1 = {"Analyse": 6, "Apply": 6, "Create": 5, "Evaluate": 4, "Remember": 5, "Understand": 6}
+        
+        assert classifier.mergeBloomsCount(count1, count2) == merged1
+        
+        count3 = {"Analyse": 0, "Apply": 0, "Create": 0, "Evaluate": 0, "Remember": 0, "Understand": 0}
+        count4 = {"Analyse": 0, "Apply": 0, "Create": 0, "Evaluate": 0, "Remember": 0, "Understand": 0}
+        merged2 = {"Analyse": 0, "Apply": 0, "Create": 0, "Evaluate": 0, "Remember": 0, "Understand": 0}
+        
+        assert classifier.mergeBloomsCount(count3, count4) == merged2
+
+
+class TestCheckCodeFormat:
+    def test_can_check_correct_formats(self):
+        assert classifier.check_code_format('COMP3900')
+        assert classifier.check_code_format('comp3900')
+        assert classifier.check_code_format('aBcD0000')
+        assert classifier.check_code_format('ZZZZ9999')
+        assert classifier.check_code_format('aaaa0909')
+    
+    def test_can_check_incorrect_formats(self):
+        assert not classifier.check_code_format('COMP3900!')
+        assert not classifier.check_code_format('COMP390')
+        assert not classifier.check_code_format('COMP.3900')
+        assert not classifier.check_code_format('abc1234')
+        assert not classifier.check_code_format('COMP39000')
+        assert not classifier.check_code_format('abcde3900')
+        assert not classifier.check_code_format('!COMP3900')
